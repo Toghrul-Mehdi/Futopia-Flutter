385625de522de8df675de67833afbbaba06496e2
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-10 12:52:54 +0400

    feat: Create reusable search bar widget - Placed in header and will be used globally - Added prefix icon and hint text - Clean material design

---
+++ lib/features/home/widgets/search_bar_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../../../core/constants/app_colors.dart';
+
+class SearchBarWidget extends StatelessWidget {
+  const SearchBarWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColors.cardBackground,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: TextField(
+        style: const TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+          hintText: 'Search here',
+          hintStyle: TextStyle(color: Colors.grey[400]),
+          prefixIcon: Icon(Icons.search, color: Colors.grey[400]),
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+        ),
+      ),
+    );
+  }
+}
