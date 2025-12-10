ef27b450e93e09194fc22f137e0ed4cd71d6a2d0
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-10 12:53:02 +0400

    feat: Implement custom bottom navigation bar - Moved to shared/widgets - Added icons and labels for 4 tabs - Smooth animation on selection

---
+++ lib/shared/widgets/bottom_nav_bar.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import '../../core/constants/app_colors.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const BottomNavBar({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColors.background,
+        border: Border(
+          top: BorderSide(color: Colors.grey[800]!, width: 1),
+        ),
+      ),
+      child: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.transparent,
+        selectedItemColor: AppColors.primary,
+        unselectedItemColor: Colors.grey[400],
+        currentIndex: currentIndex,
+        onTap: onTap,
+        elevation: 0,
+        selectedFontSize: 11,
+        unselectedFontSize: 11,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event),
+            label: 'Events',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.description),
+            label: 'My booking',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
