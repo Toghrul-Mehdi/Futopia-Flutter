41dc11746221faab070d2e250b43d18b4678802e
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-10 12:51:22 +0400

    chore: Update app colors and constants - Moved colors to core/constants/app_colors.dart - Added primary, secondary and background palette - Applied new color scheme throughout app

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF22c55e);
+  static const Color background = Color(0xFF1a1f2e);
+  static const Color backgroundDark = Color(0xFF151923);
+  static const Color cardBackground = Color(0xFF2a2f3e);
+  static const Color loginBackground = Color(0xFF0f1419);
+  static const Color inputBackground = Color(0xFF1a2028);
+  static const Color greenDark = Color(0xFF166534);
+  static const Color greenMedium = Color(0xFF15803d);
+}
