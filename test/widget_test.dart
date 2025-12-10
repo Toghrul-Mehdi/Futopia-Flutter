c5cdf73d52a6e4ab330ad732e25b4ef1e8ea0aff
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-10 13:48:19 +0400

    test: Update widget tests for new UI components - Added tests for login/register screens and auth widgets - Improved coverage for bottom nav bar

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:futopia_app/app.dart';
 import 'package:futopia_app/main.dart';
 
 void main() {
