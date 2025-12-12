af0c1026db2506c1e7b6291e8f9bac99821be090
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-12 19:37:39 +0400

    feat: Improve categories section UI
    Refactored grid layout for consistent spacing
    
    Updated icon style and text formatting
    
    Improved section responsiveness

--- lib/features/home/widgets/categories_section.dart
+++ lib/features/home/widgets/categories_section.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../../core/constants/app_colors.dart';
 import 'category_card.dart';
-
 class CategoriesSection extends StatelessWidget {
   const CategoriesSection({Key? key}) : super(key: key);
 
