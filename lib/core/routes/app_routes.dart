29b1d8d4a78c8de147937e9b90189651c5589447
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-12 19:33:48 +0400

    refactor: Update route mappings with new screens
    Added route entries for new event and category screens
    
    Refactored route naming to maintain consistent conventions
    
    Improved navigation structure to reduce redundant logic

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../features/auth/screens/login_screen.dart';
 import '../../features/home/screens/home_screen.dart';
-
 class AppRoutes {
   static const String login = '/login';
   static const String home = '/home';
