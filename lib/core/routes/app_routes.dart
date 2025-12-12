b524b50bf8c60e7b56c85713880d759f43a4e713
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-13 00:00:10 +0400

    feat: Refactor and finalize app_routes.dart - Added booking route with proper path and name - Updated route generation logic

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:futopia_app/features/event/screens/event_detail_screen.dart';
+import 'package:futopia_app/features/event/screens/event_screen.dart';
 import '../../features/auth/screens/login_screen.dart';
 import '../../features/home/screens/home_screen.dart';
+import '../../features/booking/screens/my_booking_screen.dart';
+
 class AppRoutes {
   static const String login = '/login';
   static const String home = '/home';
+  static const String events = '/events';
+  static const String eventDetail = '/event-detail';
+  static const String myBooking = '/my-booking';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -11,6 +18,12 @@ class AppRoutes {
         return MaterialPageRoute(builder: (_) => const LoginScreen());
       case home:
         return MaterialPageRoute(builder: (_) => const HomeScreen());
+      case events:
+        return MaterialPageRoute(builder: (_) => const EventScreen());
+      case eventDetail:
+        return MaterialPageRoute(builder: (_) => const EventDetailScreen());
+      case myBooking:
+        return MaterialPageRoute(builder: (_) => const MyBookingScreen());
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
