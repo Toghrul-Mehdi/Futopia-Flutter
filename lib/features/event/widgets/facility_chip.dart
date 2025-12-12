67f19303e05974d30ab396763bfd09f63ec0917c
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-12 19:37:21 +0400

    feat: Add facility chip widget
    Added new chip component with icon + label combination
    
    Styled chip to fit new UI design standards
    
    Optimized component for repeated usage

---
+++ lib/features/event/widgets/facility_chip.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import '../../../core/constants/app_colors.dart';
+
+class FacilityChip extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const FacilityChip({
+    Key? key,
+    required this.icon,
+    required this.label,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      decoration: BoxDecoration(
+        color: const Color(0xFF2a2f3e),
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(
+          color: Colors.grey[800]!,
+          width: 1,
+        ),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            icon,
+            size: 18,
+            color: AppColors.primary,
+          ),
+          const SizedBox(width: 8),
+          Text(
+            label,
+            style: const TextStyle(
+              fontSize: 14,
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
