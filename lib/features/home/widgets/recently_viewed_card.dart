bbac1b2960186cb727e9470cb037d18e5fadc833
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-12 19:38:20 +0400

    feat: Update recently viewed card
    Added placeholder visuals for missing images
    
    Updated card layout for improved readability
    
    Improved spacing and typography

--- lib/features/home/widgets/recently_viewed_card.dart
+++ lib/features/home/widgets/recently_viewed_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../../../core/constants/app_colors.dart';
-
 class RecentlyViewedCard extends StatelessWidget {
   const RecentlyViewedCard({Key? key}) : super(key: key);
 
