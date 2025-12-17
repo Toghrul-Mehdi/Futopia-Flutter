786ff6e952506ac32883718e1989ceec948c086b
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-18 00:25:36 +0400

    feat: Refine BookingCard widget design - Improved status chip colors and added cancel button

--- lib/features/booking/widgets/booking_card.dart
+++ lib/features/booking/widgets/booking_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../../core/constants/app_colors.dart';
+
 enum BookingStatus { upcoming, completed, cancelled }
 class BookingCard extends StatelessWidget {
   final String bookingId;
