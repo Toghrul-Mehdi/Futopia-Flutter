e9b87d5033a1d26b98467b808c520aed2aff1eb9
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-18 00:25:29 +0400

    feat: Enhance MyBookingScreen with booking status tabs - Added upcoming, completed, and cancelled tabs

--- lib/features/booking/screens/my_booking_screen.dart
+++ lib/features/booking/screens/my_booking_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../../../core/constants/app_colors.dart';
 import '../../../shared/widgets/bottom_nav_bar.dart';
 import '../widgets/booking_card.dart';
+
 class MyBookingScreen extends StatefulWidget {
   const MyBookingScreen({Key? key}) : super(key: key);
 
