f9ad25f83c1b899bcc150208f55c257e8256a7bd
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-18 00:27:20 +0400

    feat: Improve HeaderWidget with dynamic elements - Added search, notifications, and user avatar

--- lib/features/home/widgets/header_widget.dart
+++ lib/features/home/widgets/header_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../../core/constants/app_colors.dart';
+
 class HeaderWidget extends StatelessWidget {
   const HeaderWidget({Key? key}) : super(key: key);
 
