a0cf62c38d577243ac86435ab53088e7affacb8e
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-18 00:26:14 +0400

    feat: Add FacilityChip widget for amenities display - Reusable chips showing WiFi, Parking, Shower etc.

--- lib/features/event/widgets/facility_chip.dart
+++ lib/features/event/widgets/facility_chip.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../../core/constants/app_colors.dart';
+
 class FacilityChip extends StatelessWidget {
   final IconData icon;
   final String label;
